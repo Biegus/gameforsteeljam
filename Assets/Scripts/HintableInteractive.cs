8f0ad2e695b85d218ba9bfda747021473bf22eb8
Biegus <michalbiegaj0@wp.pl>
2022-10-29 01:12:34 +0200

    changes

---
+++ Assets/Scripts/HintableInteractive.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Game;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Game
+{
+    public abstract class HintableInteractive : MonoBehaviour, IInteractive
+    {
+        public event Action EndEvent;
+        [SerializeField] private UnityEvent onFinished;
+        private Hint hint;
+        [SerializeField] private string hintText;
+        private bool isDone = false;
+        public bool Begin()
+        {
+            hint=Hint.Spawn(hintText,new Vector2(0,3));
+            return !isDone;
+        }
+
+        protected void Finish()
+        {
+            EndEvent?.Invoke();
+                
+            onFinished?.Invoke();
+            hint.FadeOut();
+            isDone = true;
+        }
+
+        public abstract void InteractiveUpdate(bool left, bool right, Vector2 pos);
+
+
+    }
+}
--- Assets/Scripts/Lever.cs
+++ Assets/Scripts/Lever.cs
@@ -4,23 +4,14 @@ using UnityEngine.Events;
 
 namespace Game
 {
-    public class Lever : MonoBehaviour, IInteractive
+    public class Lever : HintableInteractive
     {
-        public event Action EndEvent;
         private Vector2 start;
         [SerializeField] private Vector2 desiredDirection=Vector2.down;
         [SerializeField] private float minLen=1;
         private bool active = false;
-        [SerializeField] private UnityEvent onFinished;
-        private Hint hint;
-        public bool Begin()
-        {
-            hint=Hint.Spawn("swipe down in order to release the lever",new Vector2(0,3));
-            return true;
-        }
         
-
-        public void InteractiveUpdate(bool left, bool right, Vector2 pos)
+        public override void InteractiveUpdate(bool left, bool right, Vector2 pos)
         {
                 if (left && !active)
             {
@@ -34,10 +25,7 @@ namespace Game
                 active = false;
                 if (angle < 30 && dir.sqrMagnitude >= minLen)
                 {
-                    EndEvent?.Invoke();
-                
-                    onFinished?.Invoke();
-                    hint.FadeOut();
+                    Finish();
                 }
             }
         }
