4f6bcf3747cc0f7364a11b1d1dcc0eeb3b545f49
Maticzpl <maticzpl06@gmail.com>
2022-10-29 13:33:03 +0200

    Stuff

--- Assets/Scripts/Rope.cs
+++ Assets/Scripts/Rope.cs
@@ -54,7 +54,7 @@ public class Rope : MonoBehaviour
             EndB.position = dir * Length + (Vector2)EndA.position;
         }
         //                              falling cart
-        if (dist > Length - 0.02f || EndA.position.y < EndB.position.y - 0.5f)
+        if (dist > Length || EndA.position.y < EndB.position.y - 0.5f)
         { 
             IsMaxed = true;
             Vector3[] points = { EndA.position, EndB.position };
