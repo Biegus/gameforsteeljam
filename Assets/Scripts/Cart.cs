11deb1c2874447fe84ec57512803210587c0eb96
Biegus <michalbiegaj0@wp.pl>
2022-10-30 12:20:12 +0100

    fixed

--- Assets/Scripts/Cart.cs
+++ Assets/Scripts/Cart.cs
@@ -1,17 +1,17 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.Serialization;
 
 public class Cart : MonoBehaviour
 {
     [SerializeField]
     private Vector2 MoveDirection = Vector2.right;
 
-    [SerializeField] 
-    private float Speed = 1;
     
     [SerializeField] 
     private Rope Rope;
@@ -27,7 +27,10 @@ public class Cart : MonoBehaviour
 
    private float prevIntensity;
    private Light2D light;
-       
+   [FormerlySerializedAs("curve")] [SerializeField] private AnimationCurve speed;
+   private float startX = 0;
+   [SerializeField] private Transform gizmosDown, gizmosUp;
+   [SerializeField] private Transform gizmosEnd;
     private void Awake()
     {
         particle.gameObject.SetActive(false);
@@ -40,7 +43,34 @@ public class Cart : MonoBehaviour
         prevIntensity = light.intensity;
         light.intensity = 0;
     }
-    
+
+    private void Start()
+    {
+        startX = this.transform.position.x;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!gizmosDown || !gizmosUp) return;
+        
+        float startX = this.transform.position.x;
+        if (Application.isPlaying)
+            startX = this.startX;
+        float max = speed.keys.Max(item => item.value);
+
+        Vector2 Get(float x)
+        {
+            return new Vector2(x + startX,
+                speed.Evaluate(x) * (gizmosUp.position.y - gizmosDown.position.y) + gizmosDown.position.y);
+        }
+        for (int i = 0; i <= 100; i++)
+        {
+            float x = startX + (gizmosEnd.position.x - startX) * i / ((float) 100);
+            float x2 = startX + (gizmosEnd.position.x - startX) * (i+1) / ((float) 100);
+            Gizmos.DrawLine(Get(x), Get(x2));
+        } 
+    }
+
     void Update()
     {
         awake |= Rope.IsMaxed;
@@ -61,7 +91,7 @@ public class Cart : MonoBehaviour
             }
             
             particle.gameObject.SetActive(true);
-            transform.position += (Vector3)(MoveDirection * (Speed * Time.deltaTime));
+            transform.position += (Vector3)(MoveDirection * (speed.Evaluate((this.transform.position.x-startX)) * Time.deltaTime));
         }
     }
 
